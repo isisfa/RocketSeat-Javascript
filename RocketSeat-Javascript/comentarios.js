75994f5ca5d9f9cff1368a55f9c7ba35dc9c5614
isisfa <isis.falves@gmail.com>
2021-01-23 17:05:46 +0000

    Add files

---
+++ RocketSeat-Javascript/comentarios.js
@@ -0,0 +1,10 @@
+//Aqui eu tenho um comentário em linha
+
+/*
+Aqui eu tenho um comentário multilinha ou em bloco
+
+O código abaixo escreve alguma mensagem no Devtools
+*/
+
+console.log('Bem vindos ao Starter!') //Aqui código não ignorado
+//ignorado    console.log('código')
