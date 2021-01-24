695ba3eead01db1ef0c5257e0b4aaa2aac0be6dc
isisfa <isis.falves@gmail.com>
2021-01-24 18:31:31 +0000

    Add files via upload

---
+++ RocketSeat-Javascript/002-scope.js
@@ -0,0 +1,24 @@
+    //VAR é global e também local
+    //HOISTING: ACEITA QUE EXISTE O X ANTES DE DECLARÁ-LO.
+
+    console.log(`>Existe x antes do bloco? `, x)
+{
+    var x= 0
+}
+    console.log(`Existe x depois do bloco?`, x)
+
+
+
+/* EM ARQUIVO .HTML : 
+
+<script>
+    //VAR é global e também local
+    //HOISTING: ACEITA QUE EXISTE O X ANTES DE DECLARÁ-LO.
+    console.log(`>Existe x antes do bloco? `, x)
+{
+    var x= 0
+}
+    console.log(`Existe x depois do bloco?`, x)
+</script>
+
+ */
