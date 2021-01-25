07077c0f3c384caca11c181b302675fa2593d1b6
isisfa <isis.falves@gmail.com>
2021-01-25 16:59:49 +0000

    Create 005-exercicios.js

---
+++ RocketSeat-Javascript/005-exercicios.js
@@ -0,0 +1,13 @@
+//1- Declare uma variável de nome weight
+let weight
+
+//2- Que tipo de dado é a variável acima?
+console.log (typeof weight)
+
+//3- Declare as variáveis e atribua os seus valores
+let ficha= {
+ Nome: 'Isis',
+ age: 30,
+ stars: 3.9,
+ isSubscribed: true
+}
