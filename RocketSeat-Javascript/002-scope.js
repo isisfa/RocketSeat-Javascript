1cd1c6af6cef37551236f135d719994354d16b8c
isisfa <isis.falves@gmail.com>
2021-01-25 13:11:32 +0000

    let e const

--- RocketSeat-Javascript/002-scope.js
+++ RocketSeat-Javascript/002-scope.js
@@ -1,24 +1,47 @@
-    //VAR é global e também local
+    //var é global e também local
     //HOISTING: ACEITA QUE EXISTE O X ANTES DE DECLARÁ-LO.
-
-    console.log(`>Existe x antes do bloco? `, x)
+   console.log(`>Existe x antes do bloco? `, x)
 {
     var x= 0
 }
-    console.log(`Existe x depois do bloco?`, x)
+    console.log(`>Existe x depois do bloco?`, x)
+       
+
+
+    /*VAR EM ARQUIVO .HTML : 
+    script>
+        //VAR é global e também local
+        //HOISTING: ACEITA QUE EXISTE O X ANTES DE DECLARÁ-LO.
+        console.log(`>Existe x antes do bloco? `, x)
+    {
+        var x= 0
+    }
+        console.log(`Existe x depois do bloco?`, x)
+    </script>
+    */
 
 
 
-/* EM ARQUIVO .HTML : 
+    //const e let são locais e só funcionam no escopo onde foi criada PORÉM O VALOR DE CONST NÃO PODE SER ALTERADO!!!
+    //No caso de const é preciso criar outra variavel dentro de outro bloco. E mesmo se colocar o mesmo nome serão diferentes.
+    let y= 1
+ {
+     let y= 0
+     console.log('> existe y', y) //este y só é reconhecido dentro do bloco
+ } 
+    console.log('> existe y depois do bloco?', y) //Ignora a existencia de y dentro do bloco acima
 
-<script>
-    //VAR é global e também local
-    //HOISTING: ACEITA QUE EXISTE O X ANTES DE DECLARÁ-LO.
-    console.log(`>Existe x antes do bloco? `, x)
+
+
+    //Fazer referência dentro do bloco é aceito, mesmo que esteja em vários blocos
+    let a = 2
+    console.log(a)
 {
-    var x= 0
+    {
+        {
+            a = 0
+        console.log(a) 
+        }
+    }   
 }
-    console.log(`Existe x depois do bloco?`, x)
-</script>
-
- */
+    console.log(a)
