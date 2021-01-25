20e822ba7bd3f60fa4824594ef84fb2ee2ffb35b
isisfa <isis.falves@gmail.com>
2021-01-25 16:35:55 +0000

    Create 004-array.js

---
+++ RocketSeat-Javascript/004-array.js
@@ -0,0 +1,10 @@
+const animals= [
+    'Lion', 'Monkey',
+    {name:'Cat', age: 3} //Posso incluir objetos dentro de arrays sem problemas
+]
+console.log(animals)
+console.log(animals[0])
+console.log(animals.length)
+console.log(animals[2])
+console.log(animals[2].name)
+console.log(animals[2].age)
