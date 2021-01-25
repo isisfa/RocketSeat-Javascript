513df6d4ff615380add5f8f1d60fa3c6e7d51638
isisfa <isis.falves@gmail.com>
2021-01-25 17:29:45 +0000

    Update 005-exercicios.js

--- RocketSeat-Javascript/005-exercicios.js
+++ RocketSeat-Javascript/005-exercicios.js
@@ -1,13 +1,44 @@
 //1- Declare uma variável de nome weight
 let weight
 
+
 //2- Que tipo de dado é a variável acima?
 console.log (typeof weight)
 
+
 //3- Declare as variáveis e atribua os seus valores
-let ficha= {
- Nome: 'Isis',
+let student= {
+ name: 'Isis',
  age: 30,
- stars: 3.9,
+ weight: 72.5,
  isSubscribed: true
 }
+console.log(`${student.name}, de idade ${student.age} anos, pesa ${student.weight}Kg.`)
+
+
+//4- Declare uuma variavel de tipo array de nome studentes, porém vazio.
+let students= []
+
+
+//5- Reatribua ao array acima a variável student
+students = [
+    student
+]
+console.log(students)
+
+
+//Mostre o valor 0 do array acima
+console.log(students[0])
+
+//Crie um novo student e coloque na posição 1 de students
+student[1]= {
+name: 'Fernando',
+age: 31,
+weight: 78.5,
+isSubscribed: true
+}
+console.log(student[1])
+
+//Hoisting = Por ser VAR não dá erro de referência! Reconhece o a porém não reconhece o seu valor. 
+console.log(a)
+var a= 1
