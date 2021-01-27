4cae7e6689410954fa4ddb176b207c43502a151b
isisfa <isis.falves@gmail.com>
2021-01-27 19:06:30 +0000

    Create 008-expressions.js

---
+++ RocketSeat-Javascript/008-expressions.js
@@ -0,0 +1,36 @@
+//Expressões e operadores
+
+
+//Binary
+let number = 1
+console.log(number + 2)               //Soma de dois números
+//Unary
+console.log(++ number)                //Incremento: Acrescenta mais 1
+number = 1
+console.log(-- number)                //Decremento: Diminui 1
+//Ternary                  
+console.log(true ? 'Olá' : 'Nada')   //Só existe esta possibilidade
+
+
+
+
+//Objects: 
+
+
+//new: é uma expressão que cria um novo objeto
+let name = new String('Isis')
+name.surName = "Figueiredo"
+let age = new Number(30)
+console.log(name, name.surName, age)    
+
+let date = new Date('2021-01-27')
+console.log(date)
+
+//typeOf : diz qual tipo de dado é
+console.log(`>>>>`, typeof "Isis")
+
+//Delete 
+console.log(name)
+delete name.surName
+console.log(name)
+
