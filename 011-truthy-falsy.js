3bc9ba97aad63e6346bf6d6ccb1cad55dec94328
isisfa <isis.falves@gmail.com>
2021-01-28 12:01:27 +0000

    Create 011-truthy-falsy.js

---
+++ 011-truthy-falsy.js
@@ -0,0 +1,26 @@
+// Type conversion expression for true or false
+
+/*  FALSY: 
+
+    false
+    0
+    --0
+    ""
+    null
+    undefined
+    NaN
+*/
+
+/*  TRUTHY: 
+
+    true
+    {}
+    []
+    1
+    3.23
+    "0"
+    "false"
+    -1
+    Infinity
+    -Infinity
+*/
