40d389b35f0a662d317d3d0c64396096d1e50eb3
isisfa <isis.falves@gmail.com>
2021-01-28 11:38:49 +0000

    Create 009-ternaryCond.js

---
+++ 009-ternaryCond.js
@@ -0,0 +1,6 @@
+// condicional ? true : false
+
+                                                                //Usei strings porém poderia ser qualquer variável (Object, Array, Number, ..
+let age = 17
+const canDrive = age >= 16 ? '>Can drive!' : ">Can't drive!" .)
+console.log(canDrive)
