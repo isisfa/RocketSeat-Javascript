88947b18bfc3d1db750e9e1e6768f5cf8caf4895
isisfa <isis.falves@gmail.com>
2021-01-29 11:43:46 +0000

    Update 021-sistemaDeNotasEscolares.js

--- 021-sistemaDeNotasEscolares.js
+++ 021-sistemaDeNotasEscolares.js
@@ -1,22 +1,42 @@
 
-score = 0
- let scoreA = score >= 90 && score <= 100 
- let scoreB = score >= 80 && score <= 89
- let scoreC = score >= 70 && score <= 79
- let scoreD = score >= 60 && score <= 69
- let scoreF = score >= 0 && score <= 60
+function getScore(score) {
+    let scoreA = score >= 90 && score <= 100
+    let scoreB = score >= 80 && score <= 89
+    let scoreC = score >= 70 && score <= 79
+    let scoreD = score >= 60 && score <= 69
+    let scoreF = score >= 0 && score <= 60
 
- if        (scoreA) {
-     console.log ('A')
- } else if (scoreB) {
-     console.log ('B')
- } else if (scoreC) {
-     console.log ('C')
- } else if (scoreD) {
-     console.log ('D')
- } else if (scoreF) {
-     console.log ('F')
- } else {
-    console.log('Nota errada')
+    let finalScore;
+
+    if (scoreA) {
+        finalScore = "A"
+    } else if (scoreB) {
+        finalScore = "B"
+    } else if (scoreC) {
+        finalScore = "C"
+    } else if (scoreD) {
+        finalScore = "D"
+    } else if (scoreF) {
+        finalScore = "F"
+    } else {
+        finalScore = "Nota inválida!"
+    }
+
+    return finalScore               //Quando você quer retornar um valor final fora da função.
 }
-     
+// Notas inválidas
+console.log(getScore(101))
+console.log(getScore(-1))
+// F
+console.log(getScore(0))
+console.log(getScore(1))
+console.log(getScore(45))
+// D
+console.log(getScore(60))
+console.log(getScore(61))
+// C
+console.log(getScore(75))
+// B
+console.log(getScore(85))
+// A
+console.log(getScore(95))
