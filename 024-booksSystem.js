fa621f62ef522c5bb9ef942c97f9eeac33024962
isisfa <isis.falves@gmail.com>
2021-02-02 15:35:35 +0000

    update

--- 024-booksSystem.js
+++ 024-booksSystem.js
@@ -1,8 +1,47 @@
 /*
 Buscando e contando dados em Arrays
+Baseeado no Array de livros por categoria abaixo, faça os desafios:
+
     * Contar o (número de categorias) e o (número de livros em cada categoria)
     * Contar o número de autores
     * Mostrar os livros do autor Augusto Cury
     * Transformar a função acima em uma função que irá receber o ()nome do autor e devolver os 
 
 */
+
+const booksByCategory = [
+    {
+        category: "Riqueza",
+        books: [
+            {
+                title: "Os segredos da mente milionária",
+                author: "T. Harv Eker",
+            },
+            {
+                title: "O homem mais rico da Babilôlia",
+                author: "George S. Clason",
+            },
+            {
+                title: "Pai rico, pai pobre",
+                author: "Robert T. Kiyosaki e Sharon L. Lechter",
+            },
+        ],
+    }, 
+    {
+        category: "Inteligência Emocional",
+        books: [
+            {
+                title: "Você é Insubstituível",
+                author: "Augusto Cury",
+            },
+            {
+                title: "Ansiedade - como enfrentar o mal do século",
+                author: "Augusto Cury",
+            },
+            {
+                title: "Os 7 hábitos das pessoas altamente eficazes",
+                author: "Stephen R. Covey", 
+            },
+        ],
+    },
+]
