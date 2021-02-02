8f185af0dcb2e10702225e226a7e7b961c376ad8
isisfa <isis.falves@gmail.com>
2021-02-02 11:58:21 +0000

    commit pela linha de comando

---
+++ 024-booksSystem.js
@@ -0,0 +1,8 @@
+/*
+Buscando e contando dados em Arrays
+    * Contar o (número de categorias) e o (número de livros em cada categoria)
+    * Contar o número de autores
+    * Mostrar os livros do autor Augusto Cury
+    * Transformar a função acima em uma função que irá receber o ()nome do autor e devolver os 
+
+*/
